2ec7761c28da855223d3f767c6727ae1bb06f98d
Danitaurb <Danieletontini@icloud.com>
2025-03-21 16:34:21 +0100

    Refactor input handling: replace KeyboardInputs with KeyboardListener and remove GamePanel and GameWindow classes

---
+++ src/inputs/MyMouseListener.java
@@ -0,0 +1,62 @@
+package inputs;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+import main.Game;
+import main.GameStates;
+
+public class MyMouseListener implements MouseListener, MouseMotionListener {
+
+	private Game game;
+
+	public MyMouseListener(Game game) {
+		this.game = game;
+	}
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mouseDragged'");
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mouseMoved'");
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mouseClicked'");
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mousePressed'");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mouseReleased'");
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mouseEntered'");
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mouseExited'");
+    }
+   
+	
+}
+
--- src/main/Game.java
+++ src/main/Game.java
@@ -1,16 +1,24 @@
-package main;
+ package main;
 
-import view.GamePanel;
-import view.GameWindow;
+import javax.swing.JFrame;
 
-public class Game {
+public class Game extends JFrame {
 
-    private GameWindow gameWindow;
-    private GamePanel gamePanel;
+    private static final long serialVersionUID = 1L;
+    private GameScreen gameScreen;
 
-    public Game(){
-        gamePanel = new GamePanel();
-        gameWindow = new GameWindow(gamePanel);
-        gamePanel.requestFocus();
+    public Game() {
+        setVisible(true);
+        setTitle("Phantom Siege");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setSize(1024, 576);
+        
+        gameScreen = new GameScreen();
+        add(gameScreen);
     }
+
+
+    
 }
