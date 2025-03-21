f6f31d26aee526641ebe72025574ba735a81a185
Federica <f.dimichele1@campus.uniurb.it>
2025-03-21 11:21:29 +0100

    Add Game, GamePanel, and GameWindow classes; replace Main with MainClass

---
+++ src/main/MainClass.java
@@ -0,0 +1,11 @@
+package main;
+
+public class MainClass 
+{
+    public static void main(String[] args) 
+	{
+        //System.out.println("Hello, World!");
+        //Gui gui = new Gui();
+        Game game = new Game();
+    }
+}
