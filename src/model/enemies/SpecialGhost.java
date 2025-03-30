444c123aff91ee0c0de93a284ebad0aa41ff1190
Rossi1109 <andrearossi0904@gmail.com>
2025-03-30 21:39:04 +0200

    Minor Changes

--- src/model/enemies/SpecialGhost.java
+++ src/model/enemies/SpecialGhost.java
@@ -3,13 +3,13 @@ package model.enemies;
 public class SpecialGhost extends Enemy
 {
     // Constructor
-    public SpecialGhost(double health, double speed, int reward, int damage, float x, float y, GhostType type) {
-        super(health, speed, reward, damage, x, y, type);
+    public SpecialGhost(double health, double speed, int reward, int damage, int id, float x, float y, GhostType type) {
+        super(health, speed, reward, damage, id, x, y, type);
     }
 
     // Factory method using the enum
     public static SpecialGhost createGhost(GhostType type, float x, float y) {
-        return new SpecialGhost(type.getHealth(), type.getSpeed(), type.getReward(), type.getDamage(), x, y, type.getGhostType());
+        return new SpecialGhost(type.getHealth(), type.getSpeed(), type.getReward(), type.getDamage(), type.getId(), x, y, type.getGhostType());
     }
 
 }
