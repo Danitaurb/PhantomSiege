033926b774360e44bcf1cd182da112d016665c9d
Rossi1109 <andrearossi0904@gmail.com>
2025-04-01 17:25:13 +0200

    Refactor MyMouseListener to use gameWindow for mouse event handling and enhance interaction logic for PLAYING state

--- src/controller/MyMouseListener.java
+++ src/controller/MyMouseListener.java
@@ -9,88 +9,121 @@ import view.gui.GameWindow;
 
 public class MyMouseListener implements MouseListener, MouseMotionListener {
 
-	private GameWindow game;
+	private GameWindow gameWindow;
 
-	public MyMouseListener(GameWindow game) {
-		this.game = game;
+	public MyMouseListener(GameWindow gameWindow) {
+		this.gameWindow = gameWindow;
 	}
-    
-    public void mouseDragged(MouseEvent e) {
-        // TODO Auto-generated method stub
-        
-    }
-    
-    public void mouseMoved(MouseEvent e) {
-        switch (GamePhase.gameState)
-        {
-            case MENU:
-                game.getMenu().mouseMoved(e.getX(), e.getY());
-                break;
-            case PLAYING:
-                break;
-            case SETTINGS:
-                break;
-            default:
-                break;
-        }
-    }
 
-    public void mouseClicked(MouseEvent e) {
-       if (e.getButton() == MouseEvent.BUTTON1) {
-        switch (GamePhase.gameState)
-        {
-            case MENU:
-                game.getMenu().mouseClicked(e.getX(), e.getY());
-                break;
-            case PLAYING:
-                break;
-            case SETTINGS:
-                break;
-            default:
-                break;
-        }
-       }
-    }
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		switch (GamePhase.gameState) {
+		case MENU:
+			gameWindow.getMenu().mouseDragged(e.getX(), e.getY());
+			break;
+		case PLAYING:
+			gameWindow.getPlaying().mouseDragged(e.getX(), e.getY());
+			break;
+		case SETTINGS:
+			//gameWindow.getSettings().mouseDragged(e.getX(), e.getY());
+			break;
+		default:
+			break;
+		}
 
-    @Override
-    public void mousePressed(MouseEvent e) {
-        switch (GamePhase.gameState)
-        {
-            case MENU:
-                game.getMenu().mousePressed(e.getX(), e.getY());
-                break;
-            case PLAYING:
-                break;
-            case SETTINGS:
-                break;
-            default:
-                break;
-        }
-       }
+	}
 
-    public void mouseReleased(MouseEvent e) {
-        switch (GamePhase.gameState)
-        {
-            case MENU:
-                game.getMenu().mouseReleased(e.getX(), e.getY());
-                break;
-            case PLAYING:
-                break;
-            case SETTINGS:
-                break;
-            default:
-                break;
-        }
-    }
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		switch (GamePhase.gameState) {
+		case MENU:
+			gameWindow.getMenu().mouseMoved(e.getX(), e.getY());
+			break;
+		case PLAYING:
+			gameWindow.getPlaying().mouseMoved(e.getX(), e.getY());
+			break;
+		case SETTINGS:
+			//gameWindow.getSettings().mouseMoved(e.getX(), e.getY());
+			break;
+		default:
+			break;
 
-    public void mouseEntered(MouseEvent e) {
-        // TODO Auto-generated method stub
-        
-    }
+		}
 
-    public void mouseExited(MouseEvent e) {
-        // TODO Auto-generated method stub
-    }
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		if (e.getButton() == MouseEvent.BUTTON1) {
+
+			switch (GamePhase.gameState) {
+			case MENU:
+				gameWindow.getMenu().mouseClicked(e.getX(), e.getY());
+				break;
+			case PLAYING:
+				gameWindow.getPlaying().mouseClicked(e.getX(), e.getY());
+				break;
+			case SETTINGS:
+				//gameWindow.getSettings().mouseClicked(e.getX(), e.getY());
+				break;
+			default:
+				break;
+
+			}
+
+		}
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		switch (GamePhase.gameState) {
+		case MENU:
+			gameWindow.getMenu().mousePressed(e.getX(), e.getY());
+			break;
+		case PLAYING:
+			gameWindow.getPlaying().mousePressed(e.getX(), e.getY());
+			break;
+		case SETTINGS:
+			//gameWindow.getSettings().mousePressed(e.getX(), e.getY());
+			break;
+		default:
+			break;
+
+		}
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		switch (GamePhase.gameState) {
+		case MENU:
+			gameWindow.getMenu().mouseReleased(e.getX(), e.getY());
+			break;
+		case PLAYING:
+			gameWindow.getPlaying().mouseReleased(e.getX(), e.getY());
+			break;
+		case SETTINGS:
+			//gameWindow.getSettings().mouseReleased(e.getX(), e.getY());
+			break;
+		default:
+			break;
+
+		}
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
 }
+
 
 
